2a3744defad5bc8f1ee0d6ed2159fe2c44365dfa
SMEAC <gavin.kane@hs-emden-leer.de>
2026-06-07 22:04:23 +1000

    fix: use Isaac Sim Python interpreter instead of 'python' command (missing on some systems)

--- continualTraining.sh
+++ continualTraining.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 trap "echo 'Stopped. Exiting.'; exit 0" SIGINT
 my_program() {
-    ../IsaacLab/isaaclab.sh -p scripts/rsl_rl/train.py --task=Quadruped-Locomotion-Go2 --headless --resume
+    #../IsaacLab/isaaclab.sh -p scripts/rsl_rl/train.py --task=Quadruped-Locomotion-Go2 --headless --resume
+    ../IsaacLab/isaaclab.sh -p -m torch.distributed.run --nproc_per_node=8 scripts/rsl_rl/train.py --task=Quadruped-Locomotion-Go2 --headless --distributed --resume
 }
-while true; do my_program || sleep 5; done
-
-../IsaacLab/isaaclab.sh -p scripts/rsl_rl/play.py --task=Quadruped-Locomotion-Go2 --num_envs=1
+while true; do my_program || sleep 10; done
